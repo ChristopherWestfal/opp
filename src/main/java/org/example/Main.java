04b6ea66993796092b7dffbfecffe7ca3f179fff
ChrizDope <incubusid@googlemail.com>
2024-05-29 11:11:14 +0200

    Person added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Car car1 = new Car("Audi", "A8", "Blau", 2024, 210);
         Car car2 = new Car("Volvo", "C30", "Gelb", 2012, 190);
+        Person p1 = new Person("Hans", 21, "m");
+        Person p2 = new Person("Lischen", 35, "w");
 
         car1.starts();
 
         car2.accelerates();
         car2.printSpeed();
+
+        p1.introduction();
+        p2.introduction();
     }
 }
